fd7682cd8c48cad62cef99e0752e9739df6fe10e
rawatsuri <rawatsuraj307@gmail.com>
2024-11-25 21:53:47 +0530

    4

--- src/api/config.ts
+++
@@ -1,9 +0,0 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
-
-export const handleResponse = async (response: Response) => {
-  if (!response.ok) {
-    const error = await response.json();
-    throw new Error(error.message || 'An error occurred');
-  }
-  return response.json();
-};
